69ac2a471c5482efca80aea567a3fffda9ebd67a
Gowri <gowridtp418@gmail.com>
2025-05-20 07:49:54 +0100

    upload files

---
+++ Example1.cpp
@@ -0,0 +1,25 @@
+#include"Example.hpp"
+#include<stdio.h>
+
+
+int mirror_fold() 
+{
+    if (ignition_swt == ACC)
+     {
+        if (Fold_Unfold_Mirror == UNFOLD)
+        {
+            printf("Unfold is activated\n");
+        } 
+        else 
+        {
+            printf("Fold is activated\n");
+        }
+    }
+     else 
+     {
+        printf("Vehicle is below living mode\n");
+     }
+ 
+    return 0;
+}
+ 
---
+++ Example2.cpp
@@ -0,0 +1,33 @@
+#include"Example.hpp"
+#include<stdio.h>
+int mirror_selection()
+ {
+    if (ignition_swt == ACC)
+     {
+        if (Fold_Unfold_Mirror == FOLD) 
+        {
+            printf("Cannot select mirror while fold is activated\n");
+        }
+         else
+          {
+            if (selection_Mirror == LEFT)
+            {
+                printf("LEFT mirror is activated\n");
+            } 
+            else if (selection_Mirror == RIGHT)
+            {
+                printf("RIGHT mirror is activated\n");
+            } 
+            else
+            {
+                printf("NEUTRAL is activated\n");
+            }
+        }
+    } 
+    else 
+    {
+        printf("Vehicle is below living mode\n");
+    }
+ 
+    return 0;
+}
